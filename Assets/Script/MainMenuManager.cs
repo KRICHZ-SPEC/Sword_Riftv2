a3b9c99b5c77bf4199f39f4b53a8aab420ca6805
KRICHZ-SPEC <thanakrit.chang@bumail.net>
2025-11-30 18:33:56 +0700

    setting

--- Assets/Script/MainMenuManager.cs
+++ Assets/Script/MainMenuManager.cs
@@ -4,14 +4,30 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     public string gameSceneName = "SWORDRIFT"; 
+    
+    [Header("UI Panels")]
+    public GameObject settingsPanel;
 
     public void StartGame()
     {
         SceneManager.LoadScene(gameSceneName);
     }
+    
+    public void OpenSettings()
+    {
+        if(settingsPanel != null)
+            settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        if(settingsPanel != null)
+            settingsPanel.SetActive(false);
+    }
 
     public void QuitGame()
     {
+        Debug.Log("Quitting Game...");
         Application.Quit();
     }
 }
